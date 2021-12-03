d75fbb2e5af64e2a6b2ab9990a5aa85d5bd2c692
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2021-12-03 19:30:27 +0100

    Easymatrix definition with constructor, overloaded operator<< for printing EasyMatrix

---
+++ EasyMartixStruct/src/EasyMatrix.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include "EasyMatrix.h"
+
+using namespace std;
+
+
+//Define EasyMatrix Streamer by overloading the operator <<
+ostream & operator<< (ostream & out, const EasyMatrix & matrix)
+{
+    out << " is a " << matrix.getRows() << "x";
+    out << matrix.getColumns() << " matrix:" << endl;
+
+    for (const auto & row : matrix.fData) //
+    {
+        for (const auto & col_val : row )
+            out << col_val << "\t";
+        
+        out << endl;
+    };
+    
+    return out;
+}
--- EasyMartixStruct/src/main.cpp
+++ EasyMartixStruct/src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+
 #include "EasyMatrix.h"
 
 using namespace std;
 
+ostream & operator<< (ostream & out, const EasyMatrix & matrix);
+
 int main()
 {
+    EasyMatrix matrix_a(3, 4);
+    cout << "Matrix A" <<matrix_a;
 
     return 0;
 }
