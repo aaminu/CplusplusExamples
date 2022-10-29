a99f30a4350aa5646d10dfffd606caa44b0c7038
Abdulrahman Aminu <aminuabdulrahmandayo@gmail.com>
2022-10-29 19:07:03 +0200

    Commit due to git update

--- SumFromAString/src/main.cpp
+++ SumFromAString/src/main.cpp
@@ -13,33 +13,32 @@ int main()
     getline(cin, expression);
     cout << endl;
 
-    vector<string>  values {};
+    vector<string> values{};
     string delimeter = "+";
-    size_t pos {0};
+    size_t pos{0};
     string token;
 
-    while((pos = expression.find(delimeter)) != string::npos)
+    while ((pos = expression.find(delimeter)) != string::npos)
     {
         token = expression.substr(0, pos);
-        token.erase(remove(token.begin(), token.end(), ' '), token.end());;
+        token.erase(remove(token.begin(), token.end(), ' '), token.end());
         values.push_back(token);
         expression.erase(0, pos + delimeter.length());
     }
-    expression.erase(remove(expression.begin(), expression.end(), ' '), expression.end());;
+    expression.erase(remove(expression.begin(), expression.end(), ' '), expression.end());
+    ;
     values.push_back(expression);
 
-    int sum_value {0};
-    for (int i {0}; i < values.size(); i++)
+    int sum_value{0};
+    for (int i{0}; i < values.size(); i++)
     {
         if (values[i] != "")
         {
             sum_value += stoi(values[i]);
         }
-        //cout << values[i] <<",";
+        // cout << values[i] <<",";
     }
     cout << "The sum of the Expression is: " << sum_value << endl;
 
     return 0;
 }
-
-
